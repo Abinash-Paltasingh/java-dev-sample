4e640f5830bdf5c1dfdbc9e910321b5c62b5d55b
Abinash-Paltasingh <45588602+Abinash-Paltasingh@users.noreply.github.com>
2022-01-24 16:46:43 +0530

    new line added
     some cod change

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
         System.out.println("Hello wrold"); 
-        System.out.println("Hello mr Abinash"); 
+        System.out.println("Hello mr Abinash");
+        System.out.println("Hello mr  Chandrakant"); 
     }  
 }  
