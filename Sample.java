90ba60c007aa21a4258b5955a14921c03ef1254a
Abinash-Paltasingh <45588602+Abinash-Paltasingh@users.noreply.github.com>
2022-02-04 12:37:59 +0530

    small cahnges

--- Sample.java
+++ Sample.java
@@ -3,6 +3,8 @@ class Simple{
      System.out.println("Hello Java");  
         System.out.println("Hello wrold"); 
         System.out.println("Hello mr Abinash");
-        System.out.println("Hello mr  Chandrakant"); 
+        System.out.println("Hello mr  Chandrakant");
+        System.out.println("Hello mr  rasmita");
+        
     }  
 }  
